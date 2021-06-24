513179f6879fca5b950883f7865842c093339377
Oles Bilous <bilous.oles@gmail.com>
2021-06-24 18:23:44 +0300

    Updates

--- precache-manifest.82cf192231c4fb6b402840a93da7a3f8.js
+++ precache-manifest.82cf192231c4fb6b402840a93da7a3f8.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/weatherility/static/js/runtime~main.cd16f1dc.js"
   },
   {
-    "revision": "078faf1625279742e96a",
+    "revision": "5cbe9b747a29ced84c4e",
     "url": "/weatherility/static/js/main.c2ff8009.chunk.js"
   },
   {
@@ -12,11 +12,11 @@ self.__precacheManifest = [
     "url": "/weatherility/static/js/2.3c3a0a33.chunk.js"
   },
   {
-    "revision": "078faf1625279742e96a",
-    "url": "/weatherility/static/css/main.81117a56.chunk.css"
+    "revision": "5cbe9b747a29ced84c4e",
+    "url": "/weatherility/static/css/main.ceb9a8d6.chunk.css"
   },
   {
-    "revision": "c27e80aeec8204ad5cc5a31283b2a987",
+    "revision": "93897acb963e69e061ba28c0a9b10921",
     "url": "/weatherility/index.html"
   }
 ];
